11cca1befb5d6c46b38da4f89c19f49301b8ee71
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-11 15:20:15 +0530

    Controller Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 
-
-
 function App(){
-  const [value ,setValue] = useState("Shiv bhole")
+    const [name,setName] = useState('');
+    const [password,setPassword] = useState('');
+    const [email,setEmail] = useState('');
      return(
       <div>
-        <h1>
-          Get Input  field Value
-        </h1>
-        <input type="text" value={value} onChange={(event)=>setValue(event.target.value)} placeholder="Enter User Name"/>
-        <h1>{value}</h1>
-        <button onClick = {()=>setValue("")}>
-          Clear Value
-        </button>
-        </div>
+        <h1>Controller Component</h1>
+        <form action="" method="get">
+          <input type="text" value={name} onChange={(event)=>setName(event.target.value)} placeholder="Enter Name" />
+          <br/><br/>
+          <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)} placeholder="Enter Password" />
+          <br/><br/>
+          <input type="email" value={email} onChange={(event)=>setEmail(event.target.value)} placeholder="Enter Email" />
+          <br/><br/>
+          <button>Submit</button>
+           <button onClick ={()=>{setEmail('');setPassword('');setName('')}}>Clear</button>
+
+
+          <h3>{name}</h3>
+          <h3>{password}</h3>
+          <h3>{email}</h3>
+        </form>
+      </div>
      )
   }
   
