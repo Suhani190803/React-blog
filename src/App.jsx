525d947b4a09fa084ee5ed041b1c6d379191e9c4
Suhani Mishra <suhanimishra198@gmail.com>
2025-12-11 18:02:35 +0530

    Handle ratio and drop down

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import FavFood from "./FavFood";
+import { useState } from "react";
+
 
 
 function App(){
+    const [gender,setGender] =useState('female');
     return(
       <div>
-        <h1>Handel Checkbox in React-js</h1>
-      
-        <FavFood/>
+        <h1>Handle Ratio and Dropdown</h1>
+        <h4>Select Gender</h4>
+        <input type ="radio" onChange={(event)=>setGender(event.target.value)} name="gender" 
+        value={"male"} checked={gender==female} id ="male" /> 
+        <lable htmlFor="male">Male</lable>
+        <input type ="radio" onChange={(event)=>setGender(event.target.value)} name="gender"  
+        value={"female"}  checked={gender==female} id ="female" />
+        <lable htmlFor="female">Female</lable> 
+
+        <h2>Selected Gender :{gender}</h2>
+
+
+        <br/><br/><br/>
+
+      <h4>Select City</h4>
+      <select>
+        <option value ="Banglore"> Banglore</option>
+      </select>
+        
       </div>
       
     )
